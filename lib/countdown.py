861c22573df5b2036b8b42975e5e5cb1fac9c06f
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-08-22 23:39:03 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,23 @@
-# your code goes here!
+import time
+
+
+def countdown(integer):
+    int = 0
+    while integer > int:
+        print(f"{integer} SECOND(S)!")
+        integer -= 1
+    print("HAPPY NEW YEAR!")
+
+
+print(countdown(5))
+
+
+def countdown_with_sleep(integer):
+    for i in range(integer, 0, -1):
+        print(f"{i} SECOND(S)!")
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+
+# Example usage
+countdown_with_sleep(5)
